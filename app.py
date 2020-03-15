cfd2b380bcc6b4e1488c5e6218c69a5f9045c1ed
Ubuntu <ubuntu@ip-172-31-45-229.ec2.internal>
2020-03-15 15:43:49 +0000

    with mongodB first dataset

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'Biblio'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
-
+@app.route('/get_UserReviews')
+def get_UserReviews():
+    return render_template("UserReviews.html", UserReviews=mongo.db.UserReviews.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
+
+
